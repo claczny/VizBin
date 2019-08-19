f716bac18a29b6e479abfda782280e04d1926125
Piotr Gawron <piotr.gawron@uni.lu>
2019-08-19 16:48:50 +0200

    mock of settings added so creation of settings folder do not fail

--- src/interface/VizBin/test/lu/uni/lcsb/vizbin/MainCliTest.java
+++ src/interface/VizBin/test/lu/uni/lcsb/vizbin/MainCliTest.java
@@ -4,9 +4,22 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
-import org.junit.Test;
+import org.junit.*;
+import org.mockito.Mockito;
+
+import lu.uni.lcsb.vizbin.settings.ISettings;
 
 public class MainCliTest {
+  @Before
+  public void setUp() {
+    ISettings settings = Mockito.mock(ISettings.class);
+    Main.settings = settings;
+  }
+
+  @After
+  public void tearDown() {
+    Main.settings = null;
+  }
 
   @Test
   public void testBasicProcessing() {
